a276a9afe674a4b40671ba2befa3640727ac4842
Vicky Shinde <vickyshinde30805@gmail.com>
2026-07-31 19:00:29 +0530

    Time: 15 ms (77.47%), Space: 19.6 MB (71.76%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,14 +1,6 @@
 from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        return Counter(ransomNote)<=Counter(magazine)
 
-        freq=Counter(magazine)
-        freq_1=Counter(ransomNote)
-
-        for char in ransomNote:
-            if char in magazine and freq_1<=freq:
-
-                return True
-        return False
-        
         
