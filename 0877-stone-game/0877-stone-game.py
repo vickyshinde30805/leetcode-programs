1bb3a5f11f3db5cce68de35e7e13e3ca0caf7007
Vicky Shinde <vickyshinde30805@gmail.com>
2026-08-02 11:54:29 +0530

    Time: 260 ms (31.15%), Space: 52.8 MB (28.59%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,7 +1,16 @@
 class Solution:
-    def stoneGame(self, piles: List[int]) -> bool:
-        return True
-        
-        
-
-        
+    def stoneGame(self, piles):
+        n=len(piles)
+        dp=[[None]*n for i in range(n)]
+        def solve(i,j):
+            if i==j:
+                return piles[i]
+            if dp[i][j]!=None:
+                return dp[i][j]
+            left=piles[i]-solve(i+1,j)
+            right=piles[j]-solve(i,j-1)
+            dp[i][j]=max(left,right)
+            return dp[i][j]
+        return solve(0,n-1)>=0
+            
+       
