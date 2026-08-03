0b02ad4093795fff0baab523931b6085dc28c694
Vicky Shinde <vickyshinde30805@gmail.com>
2026-08-03 13:16:24 +0530

    Time: 917 ms (51.3%), Space: 220.5 MB (20.29%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -1,22 +1,20 @@
 class Solution:
-    s = ["Bob", "Tie", "Alice"]
-    def stoneGameIII(self, A: List[int]) -> str:
-        n = len(A)
-
-        @cache
-        def maxDiff(i: int) -> int:
-            if i == n: return 0
-            a = b = c = -5e7
-
-            if i < n:
-                a = A[i] - maxDiff(i + 1)
-            if i + 1 < n:
-                b = A[i] + A[i + 1] - maxDiff(i + 2)
-            if i + 2 < n:
-                c = A[i] + A[i + 1] + A[i + 2] - maxDiff(i + 3)
-
-            return max(a, b, c)
-
-        d = maxDiff(0)
-        return self.s[(d > 0) - (d < 0) + 1]
+    r=["Bob","Tie","Alice"]
+    def stoneGameIII(self, s: List[int]) -> str:
+        n=len(s)
         
+        @cache
+        def solve(i):
+            if i==n:return 0
+            a=b=c=-5e7
+
+            if i<n:
+                a=s[i]-solve(i+1)
+            if i+1<n:
+                b=s[i]+s[i+1]-solve(i+2)
+            if i+2<n:
+                c=s[i]+s[i+1]+s[i+2]-solve(i+3)
+            return max(a,b,c)
+
+        b=solve(0)
+        return self.r[(b>0)-(b<0)+1]
